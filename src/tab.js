6af0070755d38705d4f93e4b3320ccf4d165f962
Brian Murray <bmurray1228@gmail.com>
2021-07-12 23:00:48 -0500

    Add vim keys for insert, append, delete, and movement

--- src/tab.js
+++ src/tab.js
@@ -72,6 +72,8 @@ class Tab extends React.Component {
     for (let i = 0; i < 60; i++) {
       beats.push([...EMPTY_BEAT]);
     }
+    // TODO: Add ability to select multiple beats.
+    // TODO: Add ability to annotate a beat range.
     this.state = {
       beats,
       selection: {
@@ -86,7 +88,7 @@ class Tab extends React.Component {
   }
 
   onKeyDown(e) {
-    if (isArrowKey(e.keyCode)) {
+    if (isArrowKey(e.keyCode) || isVimMovementKey(e.keyCode)) {
       // Only process arrow keys.
       const curBeat = this.state.selection.beat;
       const curFret = this.state.selection.fret;
@@ -96,15 +98,19 @@ class Tab extends React.Component {
       }
       switch (e.keyCode) {
         case 37: // Left
+        case 72: // H
           this.select(curBeat - 1, curFret);
           break;
         case 38: // Up
+        case 75: // K
           this.select(curBeat, curFret - 1);
           break;
         case 39: // Right
+        case 76: // L
           this.select(curBeat + 1, curFret);
           break;
         case 40: // Down
+        case 74: // J
           this.select(curBeat, curFret + 1);
           break;
         default:
@@ -122,6 +128,10 @@ class Tab extends React.Component {
   }
 
   onKeyPressed(e) {
+    if (this.state.selection.beat === null ||
+        this.state.selection.fret === null) {
+      return;
+    }
     if (!isNaN(parseInt(e.key, 10))) {
       // Only process number keys.
       let pendingValue = this.state.selection.pendingValue ?? '';
@@ -134,13 +144,43 @@ class Tab extends React.Component {
         }
       });
     }
+    else if (e.key === 'i') {
+      // Insert beat before the current selection.
+      this.commitPendingValue();
+      const beats = this.cloneBeats();
+      beats.splice(this.state.selection.beat, 0, [...EMPTY_BEAT]);
+      this.setState({beats});
+    } else if (e.key === 'a') {
+      // Insert beat after the current selection.
+      this.commitPendingValue();
+      const beats = this.cloneBeats();
+      beats.splice(this.state.selection.beat + 1, 0, [...EMPTY_BEAT]);
+      this.setState({beats});
+      this.select(this.state.selection.beat + 1, this.state.selection.fret);
+    } else if (e.key === 'd') {
+      // Delete the current beat.
+      this.commitPendingValue();
+      const beats = this.cloneBeats();
+      beats.splice(this.state.selection.beat, 1);
+      this.setState({beats});
+    }
   }
 
   commitPendingValue() {
+    if (this.state.selection.pendingValue === null) {
+      return;
+    }
     const beats = this.cloneBeats();
     beats[this.state.selection.beat][this.state.selection.fret] =
         this.state.selection.pendingValue;
-    this.setState({beats});
+    this.setState({
+      beats,
+      selection: {
+        beat: this.state.selection.beat,
+        fret: this.state.selection.fret,
+        pendingValue: null,
+      }
+    });
   }
 
   cloneBeats() {
@@ -165,7 +205,7 @@ class Tab extends React.Component {
       beats.push([...EMPTY_BEAT]);
     }
     this.setState({
-      selection:  {beat, fret},
+      selection:  {beat, fret, pendingValue: null},
       beats,
     });
   }
@@ -198,6 +238,13 @@ class Tab extends React.Component {
 
 function isArrowKey(keyCode) {
   return keyCode >= 37 && keyCode <= 40;
+}
+
+function isVimMovementKey(keyCode) {
+  return keyCode === 72 ||
+      keyCode === 74 ||
+      keyCode === 75 ||
+      keyCode === 76;
 }
 
 export default Tab;
